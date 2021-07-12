b49ca32437b0254e2906c0098bd14ec912b529a8
lawrence <whthupnd@gmail.com>
2021-07-12 10:03:06 +0800

    Added message

--- fct-app/src/constants/index.js
+++ fct-app/src/constants/index.js
@@ -1,5 +1,5 @@
 export const constants = {
     REPO_URL: 'https://github.com/ashamn/frontend-developer-test',
-    MESSAGE: '',
+    MESSAGE: 'What do you call a bear that has no teeth?',
     NAME: 'Lawrence Charles Fetalvero'
 };
